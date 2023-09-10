75c8df110dda0b827a2979b633efbf42636f3bd8
Adinai <RYSMENDEEVAADINAI@GMAIL.COM>
2023-09-10 17:23:55 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useState} from "react";
 
 function App() {
+
+    const[tasks, setTasks] = useState([
+        {
+            id: 1,
+            title: 'Buy car',
+            isDone: false,
+            isImportant: false
+        },
+        {
+            id: 2,
+            title: 'Buy toy',
+            isDone: false,
+            isImportant: false
+        }
+
+    ])
+    const [filter, setFilter] = useState([""])
+
+
+    const addTask = (e) => {
+        e.preventDefault()
+         setTasks([
+            ...tasks,
+            {
+                id: tasks.length ? tasks[tasks.length -1].id + 1 : 1,
+                title: e.target[0].value,
+                isDone: false,
+                isImportant: false
+            }
+
+        ])
+       return e.target[0].value = ''
+    }
+
+    const deleteTask = (id) => {
+        return setTasks(
+            tasks.filter((item) => item.id !== id)
+        )
+    }
+
+    const doneTask =(id) => {
+        return setTasks(tasks.map((item) => {
+            if (item.id === id){
+                return {...item,  isDone: !item.isDone}
+            }
+            return item
+        }))
+    }
+
+    const importantTask =(id) => {
+        return setTasks(tasks.map((item) => {
+            if (item.id === id){
+                return {...item,  isImportant: !item.isImportant}
+            }
+            return item
+        }))
+    };
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <form onSubmit={addTask}>
+              <input type="text" minLength={5} maxLength={20} required/>
+              <button type="submit">Add</button>
+          </form>
+          <button type={"button"} onClick={() => setFilter("done")}>done</button>
+          <button type={"button"} onClick={() => setFilter("important")}>important</button>
+          <button type={"button"} onClick={() => setFilter("all")}>all</button>
+
+          <ul>
+              {
+                tasks.length
+                    ?
+                tasks.filter(item => {
+                    if (filter === "done"){
+                        return item.isDone
+                    }
+                    else if (filter === "important"){
+                        return item.isImportant
+                    }
+                    else {
+                        return item
+                    }
+              }).map((item) => (
+                        <li>
+                            <span style={{
+                               textDecoration: item.isDone ? 'line-through' : '',
+                               background: item.isImportant ? 'purple' : ''
+                            }}>
+                      {item.title}
+                  </span>
+                            <div>
+                                <button
+                                    onClick={() => doneTask(item.id)}
+                                >
+                                    done
+                                </button>
+                                <button
+                                    onClick={() => importantTask(item.id)}
+                                >
+                                    important
+                                </button>
+                                <button
+                                    onClick={() => deleteTask(item.id)}
+                                >
+                                    delete
+                                </button>
+                            </div>
+                        </li>
+                ))
+                    :
+                    <li>Tasks is empty</li>
+              }
+
+          </ul>
+      </div>
+
   );
 }
 
